7a33fdb319fe6a7549be4709112b9182388baf20
Angie Rodriguez <angrodri902@gmail.com>
2019-10-21 20:58:05 -0700

    Updates

---
+++ precache-manifest.e48542245343c9b4bc40b57cac0c18d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbbe6ddc471b7f740b7e8457fe938a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94174ce2b82758727219",
+    "url": "/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "1cc6ec76e7b15e2fb5fd",
+    "url": "/static/css/main.3b65a993.chunk.css"
+  },
+  {
+    "revision": "94174ce2b82758727219",
+    "url": "/static/js/2.726ded5f.chunk.js"
+  },
+  {
+    "revision": "1cc6ec76e7b15e2fb5fd",
+    "url": "/static/js/main.d9abe1f9.chunk.js"
+  },
+  {
+    "revision": "32ad726e43f9be3a7047",
+    "url": "/static/js/runtime-main.c07b4c9e.js"
+  }
+]);
