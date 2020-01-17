a6ff5fb3163a6596e6f9094517bf7ee6750d8ec5
Angie Rodriguez <angrodri902@gmail.com>
2020-01-17 12:52:24 -0800

    Update portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,83 +1,29 @@
-import React, { Component } from 'react';
-import { Card } from 'react-bulma-components';
-import { Content } from 'react-bulma-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import portfolioData from './../portfolioData';
 
-class Portfolio extends Component {
-  render() {
+const Portfolio = () =>  {
     return (
-      <section className="container">
+      <section className="container portfolio-container">
       <h1 className="title is-1">Projects</h1>
-        <div className="columns is-multiline is-centered">
-          <div className="column card-spacing is-two-thirds-tablet is-half-desktop">
-            <Card className="img">
-              <Card.Image src="./assets/img/favorite-fonts-min.png" />
-              <Card.Content>
-                <Content className="has-text-centered">
-                <h3>Favorite Fonts</h3>
-                A Google Fonts landing page clone built with plain JavaScript and FlexBox<br />
-                <em>(Vanilla JavaScript, HTML5, CSS3, FlexBox)</em><br />
-                <a href="https://github.com/ARodriguezHacks/favorite-fonts" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} size="2x" className="icon-spacing" />View project on Github</a>
-                  <br />
-                  View demo on<br /> 
-                  <a href="https://angies-favorite-fonts.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="has-text-link"> Heroku</a>
-                  <br />
-                </Content>
-              </Card.Content>
-            </Card>
+        {portfolioData.map( item => {
+          return (
+          <div className="project is-clearfix" key={item.id}>
+            <a href={item.deployed} target="_blank" rel="noopener noreferrer"><img src={item.image} alt={item.alt} className="card-display is-pulled-left" /></a>
+            <div className="project-text is-pulled-left">
+              <h2 className="is-size-2">{item.title}</h2>
+              <h5 className="is-size-6">{item.description}</h5>
+              <div className="logo-section">
+                <a href={item.github} target="_blank" rel="noopener noreferrer" className="logo-margin">
+                  <img src="./assets/icons/001-github.png" alt="Github logo"/></a>
+                <a href={item.deployed} target="_blank" rel="noopener noreferrer">
+                <img src={item.logo} alt={item.deployedAlt}/></a>
+              </div>
+            </div>
           </div>
-          <div className="column card-spacing is-two-thirds-tablet is-half-desktop">
-            <Card className="img">
-              <Card.Image src="./assets/img/tb-portfolio.png" />
-              <Card.Content>
-                <Content className="has-text-centered">
-                <h3>TravelBucket</h3>
-                A Reddit-inspired social media app for sharing posts and comments within a travel blog community.<br />
-                <em>(Ruby, Rails, Bootstrap 5)</em><br />
-                <a href="https://github.com/ARodriguezHacks/travel-bucket-app" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} size="2x" className="icon-spacing" />View project on Github</a>
-                <br />
-                  View demo on<br /> 
-                  <a href="https://travelbucketapp.herokuapp.com" target="_blank" rel="noopener noreferrer" className="has-text-link"> Heroku</a>
-                </Content>
-              </Card.Content>
-            </Card>
-          </div>
-          <div className="column card-spacing is-two-thirds-tablet is-half-desktop">
-            <Card className="img">
-              <Card.Image src="./assets/img/parle-chat-portfolio.png" />
-              <Card.Content>
-                <Content className="has-text-centered">
-                  <h3>Parle!</h3>
-                  A React.js chat app built with a realtime database.<br />
-                  <em>(HTML5, Reactjs (JavaScript), Bootstrap 5)</em><br />
-                  <a href="https://github.com/ARodriguezHacks/parle-app" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} size="2x" className="icon-spacing" />View project on Github</a>
-                  <br />
-                  View demo on<br /> 
-                  <a href="https://my-firebase-react-chatapp.web.app" target="_blank" rel="noopener noreferrer" className="has-text-link"> Firebase</a>
-                </Content>
-              </Card.Content>
-            </Card>
-          </div>
-          <div className="column card-spacing is-two-thirds-tablet is-half-desktop">
-            <Card className="img">
-              <Card.Image src="./assets/img/magicpongupdate-4-copy.png" />
-              <Card.Content>
-                <Content className="has-text-centered">
-                  <h3>Magic Pong</h3>
-                  A recreation of the classic Pong game.<br />
-                  <em>(HTML5, Vanilla JavaScript)</em><br />
-                  <a href="https://github.com/ARodriguezHacks/magicpong" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} size="2x" className="icon-spacing" />View project on Github</a>
-                  <br />
-                  View demo on 
-                  <a href="https://magic-pong-game.herokuapp.com" target="_blank" rel="noopener noreferrer" className="has-text-link"> Heroku</a>
-                </Content>
-              </Card.Content>
-            </Card>
-          </div>
-        </div>
+          );
+        })}
       </section>
     )
-  }
 }
 
 export default Portfolio;
---
+++ src/portfolioData.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    id: 1,
+    image: '/assets/img/tb-portfolio.png',
+    alt: 'Travel Bucket Project',
+    title: 'Travel Bucket',
+    description: 'A Reddit-inspired social media app for sharing posts and comments within a travel blog community.',
+    github: 'https://github.com/ARodriguezHacks/travel-bucket-app',
+    logo: '/assets/icons/002-heroku.png',
+    deployedAlt: 'Heroku Logo',
+    deployed: 'https://travelbucketapp.herokuapp.com'
+  },
+  {
+    id: 2,
+    image: '/assets/img/parle-chat-portfolio.png',
+    alt: 'Parle! Chat App Project',
+    title: 'Parle!',
+    description: 'A React.js chat app built with a realtime database',
+    github: 'https://github.com/ARodriguezHacks/parle-app',
+    logo: '/assets/icons/icons8-firebase-50.png',
+    deployedAlt: 'Firebase Logo',
+    deployed: 'https://my-firebase-react-chatapp.web.app'
+  },
+  {
+    id: 3,
+    image: '/assets/img/favorite-fonts-min.png',
+    alt: 'Favorite Fonts Project',
+    title: 'Favorite Fonts',
+    description: 'A Google Fonts landing page clone built with plain JavaScript and FlexBox',
+    github: 'https://github.com/ARodriguezHacks/favorite-fonts',
+    logo: '/assets/icons/002-heroku.png',
+    deployedAlt: 'Heroku Logo',
+    deployed: 'https://angies-favorite-fonts.herokuapp.com/'
+  }
+]
